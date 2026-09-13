46705a8a1b219bb4a2092e74b5e0905093f2a83a
Micar-Arguelles <micarshanearguelles@gmail.com>
2026-09-14 03:50:10 +0800

    updated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.bookcollection',
-  appName: 'book-collection',
+  appName: 'Book Collection Manager',
   webDir: 'dist'
 };
 
